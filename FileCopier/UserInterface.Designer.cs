153b6af19661fad57ca45a15943aa83a271d6bef
Jordan Mryyan <jmryyan@ksu.edu>
2018-01-11 12:29:28 -0600

    Initial commit, broken if file exists

---
+++ FileCopier/UserInterface.Designer.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace FileCopier
+{
+    partial class UserInterface
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uxProgressLabel = new System.Windows.Forms.Label();
+            this.uxDest = new System.Windows.Forms.TextBox();
+            this.uxSource = new System.Windows.Forms.TextBox();
+            this.uxDestButton = new System.Windows.Forms.Button();
+            this.uxSourceButton = new System.Windows.Forms.Button();
+            this.uxProgressBar = new System.Windows.Forms.ProgressBar();
+            this.uxCopyButton = new System.Windows.Forms.Button();
+            this.uxSourceBrowser = new System.Windows.Forms.FolderBrowserDialog();
+            this.uxDestBrowser = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // uxProgressLabel
+            // 
+            this.uxProgressLabel.AutoSize = true;
+            this.uxProgressLabel.Location = new System.Drawing.Point(123, 96);
+            this.uxProgressLabel.Name = "uxProgressLabel";
+            this.uxProgressLabel.Size = new System.Drawing.Size(24, 13);
+            this.uxProgressLabel.TabIndex = 14;
+            this.uxProgressLabel.Text = "0/0";
+            // 
+            // uxDest
+            // 
+            this.uxDest.Location = new System.Drawing.Point(12, 44);
+            this.uxDest.Name = "uxDest";
+            this.uxDest.ReadOnly = true;
+            this.uxDest.Size = new System.Drawing.Size(221, 20);
+            this.uxDest.TabIndex = 13;
+            // 
+            // uxSource
+            // 
+            this.uxSource.Location = new System.Drawing.Point(12, 12);
+            this.uxSource.Name = "uxSource";
+            this.uxSource.ReadOnly = true;
+            this.uxSource.Size = new System.Drawing.Size(221, 20);
+            this.uxSource.TabIndex = 12;
+            // 
+            // uxDestButton
+            // 
+            this.uxDestButton.Location = new System.Drawing.Point(239, 41);
+            this.uxDestButton.Name = "uxDestButton";
+            this.uxDestButton.Size = new System.Drawing.Size(32, 23);
+            this.uxDestButton.TabIndex = 11;
+            this.uxDestButton.Text = "...";
+            this.uxDestButton.UseVisualStyleBackColor = true;
+            this.uxDestButton.Click += new System.EventHandler(this.uxDestButton_Click);
+            // 
+            // uxSourceButton
+            // 
+            this.uxSourceButton.Location = new System.Drawing.Point(239, 12);
+            this.uxSourceButton.Name = "uxSourceButton";
+            this.uxSourceButton.Size = new System.Drawing.Size(32, 23);
+            this.uxSourceButton.TabIndex = 10;
+            this.uxSourceButton.Text = "...";
+            this.uxSourceButton.UseVisualStyleBackColor = true;
+            this.uxSourceButton.Click += new System.EventHandler(this.uxSourceButton_Click);
+            // 
+            // uxProgressBar
+            // 
+            this.uxProgressBar.Location = new System.Drawing.Point(12, 70);
+            this.uxProgressBar.Name = "uxProgressBar";
+            this.uxProgressBar.Size = new System.Drawing.Size(259, 23);
+            this.uxProgressBar.TabIndex = 9;
+            // 
+            // uxCopyButton
+            // 
+            this.uxCopyButton.Location = new System.Drawing.Point(12, 126);
+            this.uxCopyButton.Name = "uxCopyButton";
+            this.uxCopyButton.Size = new System.Drawing.Size(259, 23);
+            this.uxCopyButton.TabIndex = 8;
+            this.uxCopyButton.Text = "Copy";
+            this.uxCopyButton.UseVisualStyleBackColor = true;
+            this.uxCopyButton.Click += new System.EventHandler(this.uxCopyButton_ClickAsync);
+            // 
+            // UserInterface
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 163);
+            this.Controls.Add(this.uxProgressLabel);
+            this.Controls.Add(this.uxDest);
+            this.Controls.Add(this.uxSource);
+            this.Controls.Add(this.uxDestButton);
+            this.Controls.Add(this.uxSourceButton);
+            this.Controls.Add(this.uxProgressBar);
+            this.Controls.Add(this.uxCopyButton);
+            this.Name = "UserInterface";
+            this.Text = "Copy Files";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+
+        #endregion
+        private Label uxProgressLabel;
+        private TextBox uxDest;
+        private TextBox uxSource;
+        private Button uxDestButton;
+        private Button uxSourceButton;
+        private ProgressBar uxProgressBar;
+        private Button uxCopyButton;
+        private FolderBrowserDialog uxSourceBrowser;
+        private FolderBrowserDialog uxDestBrowser;
+    }
+}
+
---
+++ FileCopier/UserInterface.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileCopier
+{
+    public partial class UserInterface : Form
+    {
+        private bool _operationInProgress = false;
+        public UserInterface()
+        {
+            InitializeComponent();
+        }
+
+        private void uxSourceButton_Click(object sender, EventArgs e)
+        {
+            if (uxSourceBrowser.ShowDialog() == DialogResult.OK)
+            {
+                uxSource.Text = uxSourceBrowser.SelectedPath;
+            }
+        }
+
+        private void uxDestButton_Click(object sender, EventArgs e)
+        {
+            if (uxDestBrowser.ShowDialog() == DialogResult.OK)
+            {
+                uxDest.Text = uxDestBrowser.SelectedPath;
+            }
+        }
+
+        private void UserInterface_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // We don't want to stop windows from trying to close down
+            if (e.CloseReason == CloseReason.WindowsShutDown) return;
+
+            if (_operationInProgress)
+            {
+                DialogResult result = MessageBox.Show("An operation is currently in progress, are you sure you would like to quit?", "Confirm exit", MessageBoxButtons.YesNo);
+
+                if (result == DialogResult.No)
+                    e.Cancel = true;
+                else
+                    return;
+            }
+        }
+
+
+        /// <summary>
+        /// Toggles all the UI buttons, and resets the progress bar
+        /// </summary>
+        private void ResetUI()
+        {
+            ToggleUIButtons();
+            ResetProgress();
+        }
+
+        /// <summary>
+        /// Resets the progress bar to its idle state
+        /// </summary>
+        private void ResetProgress()
+        {
+            uxProgressBar.Value = 0;
+            uxProgressBar.Maximum = CalculateNumFiles(uxSource.Text, true);
+            uxProgressLabel.Text = "0/" + uxProgressBar.Maximum;
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="location"></param>
+        /// <param name="searchSubfolders"></param>
+        /// <returns></returns>
+        public static int CalculateNumFiles(string location, bool searchSubfolders)
+        {
+            SearchOption option = (searchSubfolders) ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly;
+            return Directory.GetFiles(location, "*.*", option).Length;
+        }
+
+        /// <summary>
+        /// Sets all buttons to the opposite enabled status
+        /// </summary>
+        private void ToggleUIButtons()
+        {
+            uxSourceButton.Enabled = !uxSourceButton.Enabled;
+            uxDestButton.Enabled = !uxDestButton.Enabled;
+            uxCopyButton.Enabled = !uxCopyButton.Enabled;
+        }
+
+
+        private void RemoveEmptyFolders(string root)
+        {
+            foreach (var directory in Directory.GetDirectories(root))
+            {
+                RemoveEmptyFolders(directory);
+                if (Directory.GetFiles(directory).Length == 0 && Directory.GetDirectories(directory).Length == 0)
+                {
+                    Directory.Delete(directory, false);
+                }
+            }
+        }
+
+        private async Task<bool> MoveFolder(DirectoryInfo root, string savePath)
+        {
+            FileInfo[] files = null;
+            DirectoryInfo[] subDirs = null;
+
+            try
+            {
+                files = root.GetFiles("*.pdf");
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            if (files != null)
+            {
+                if (files.Count() > 0)
+                {
+
+                    foreach (FileInfo file in files)
+                    {
+                        try
+                        { 
+                              File.Move(file.FullName, Path.Combine(savePath, file.Name));
+                        }
+                        catch (IOException ex)
+                        {
+                            DirectoryInfo dir = new DirectoryInfo(savePath);
+                            FileInfo[] saveFiles = dir.GetFiles(file.Name + "*");
+
+                            string fileNameWithoutExt = file.Name.Substring(0, file.Name.LastIndexOf('.'));
+                            string newSavePath = Path.Combine(savePath, String.Format("{0}({1}).pdf", fileNameWithoutExt, saveFiles.Count()));
+                            File.Move(file.FullName, newSavePath);
+                        }
+
+                        if (InvokeRequired)
+                        {
+                            BeginInvoke(new Action(() =>
+                            {
+                                uxProgressBar.Value++;
+                                uxProgressLabel.Text = uxProgressBar.Value + "/" + uxProgressBar.Maximum;
+                            }));
+                        }
+                    }
+                }
+
+                subDirs = root.GetDirectories();
+
+                foreach (DirectoryInfo dirInfo in subDirs)
+                {
+                    await MoveFolder(dirInfo, savePath);
+                }
+            }
+
+            return true;
+        }
+
+        private async void uxCopyButton_ClickAsync(object sender, EventArgs e)
+        {
+            if (uxSource.Text != String.Empty && uxDest.Text != String.Empty)
+            {
+                ResetUI();
+                _operationInProgress = true;
+
+                DirectoryInfo dir = new DirectoryInfo(uxSource.Text);
+                Task<bool> test = await Task.Factory.StartNew(() => MoveFolder(dir, uxDest.Text));
+
+                RemoveEmptyFolders(uxSource.Text);
+                MessageBox.Show("Finished dumping files!");
+
+                _operationInProgress = false;
+                ResetUI();
+            }
+        }
+    }
+}
